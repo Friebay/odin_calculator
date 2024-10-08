6016f09811b771028d5bbdb76a2a6f3664c2f7e9
Friebay <frybaylt@gmail.com>
2024-10-08 15:30:10 +0300

    Skaičiai ir veiksmas į kintamuosius

--- app.js
+++ app.js
@@ -28,3 +28,27 @@ function add(a, b) {
   console.log(divide(5, 3));
   console.log(divide(5, 0));
   
+
+  // Variables for the calculator operation
+let firstNumber = 3;    // Example: the first number
+let operator = '+';     // Example: the operator
+let secondNumber = 5;   // Example: the second number
+
+// Testing the operation with a simple calculation
+function calculate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+    default:
+      return "Invalid operator";
+  }
+}
+
+// Output test
+console.log(calculate(firstNumber, operator, secondNumber));  // 8 in this case (3 + 5)
